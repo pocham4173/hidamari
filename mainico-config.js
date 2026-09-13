da4d7a8ca7a5991afc75f7f98314fc95fdd2d201
pocham4173 <pocham4173@gmail.com>
2026-09-14 05:33:07 +0900

    無料App Checkの公開キーを設定する

--- mainico-config.js
+++ mainico-config.js
@@ -2,4 +2,4 @@
    公開鍵なので配信して構いません。秘密鍵はこのリポジトリへ置かないでください。 */
 window.MAINICO_VAPID_KEY = window.MAINICO_VAPID_KEY || 'BNU4nWgAISADaOClc9FuGgVzO3iwtkakn5mQADaPE4ezhcrbkWad-trI1OKli1QnpXoL9GgInvCcFFkymV0M_xQ';
 /* Firebase App Check に登録した reCAPTCHA Enterprise のサイトキー。 */
-window.MAINICO_RECAPTCHA_SITE_KEY = window.MAINICO_RECAPTCHA_SITE_KEY || '';
+window.MAINICO_RECAPTCHA_SITE_KEY = window.MAINICO_RECAPTCHA_SITE_KEY || '6LdvSrktAAAAAGBxlUhBFFb7yn6inWPKJIM2vK7x';
